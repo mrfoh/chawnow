caa7cf9424362cbb928cafb3415371f3d084c168
Patrick Foh <patrickfoh@gmail.com>
2014-03-03 23:10:56 +0100

    "New"

--- app/handlers.php
+++ app/handlers.php
@@ -7,8 +7,10 @@
 |
 */
 
+//User Events
 Event::listen('user.registered', 'UserListener@registered');
 
+//Order Events
 Event::listen('order.placed', 'OrderListener@placed');
 Event::listen('order.verified', 'OrderListener@verified');
 Event::listen('order.confirm', 'OrderListener@confirm');
